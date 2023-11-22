323214cddaec307258104916580ea189bdc48807
ChesterRougton <151744851+ChesterRougton@users.noreply.github.com>
2023-11-22 07:21:35 -0500

    Add files via upload

---
+++ headerTemp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "UTF-8">
+	<meta name-"viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Shan Tang</title>
+</head>
+
+<body>
+	<div class = "topDiv"></div>
+	<header>
+		<ul>
+			<li><a href="index.html">Shan T.</a></li>
+			<li><a href="Resume.html">Resume</a></li>
+			<li><a href="#projects">Projects</a></li>
+		</ul>
+	</header>
